f6fd6ac8db29dfacfeac3505604ff34f4b39ac94
christheboss894 <35890103+christheboss894@users.noreply.github.com>
2018-01-31 19:30:43 +1100

    0.0.1

---
+++ BeltsPlusPlus_0.0.1/data.lua
@@ -0,0 +1,4 @@
+
+require("prototypes.green-belt-recipe")
+require("prototypes.green-belt-item")
+require("prototypes.green-belt")
---
+++ BeltsPlusPlus_0.0.1/locale.lua
@@ -0,0 +1,6 @@
+[recipe-name]
+uber-transport-belt=Uber Transport Belt
+[entity-name]
+uber-transport-belt=Uber Transport Belt
+[item-name]
+uber-transport-belt=Uber Transport Belt
